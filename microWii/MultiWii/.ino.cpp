ac3b2e6405eb902288953afd43493039ec4c10c1
Steven Swanson <swanson@cs.ucsd.edu>
2016-04-21 13:29:37 -0700

    cleanup

--- microWii/MultiWii/.ino.cpp
+++
@@ -1,9 +0,0 @@
-//This is a automatic generated file
-//Please do not modify this file
-//If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-04-20 22:21:08
-
-#include "Arduino.h"
-
-#include "MultiWii.ino"
-
